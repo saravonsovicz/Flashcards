8adeb92a4954499b647f996d690a61e7a648148b
saravonsovicz <149506316+saravonsovicz@users.noreply.github.com>
2024-09-25 17:30:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Chernobyl',
+    'O que aconteceu em Chernobyl?',
+    'Em 26 de abril de 1986, o reator quatro da Usina Nuclear de Chernobyl explodiu, liberando radiação. Isso resultou na evacuação de 116 mil pessoas e causou doenças e mortes.'
+)
+
+criaCartao(
+    'Chernobyl',
+    'Como a fauna e flora se adaptaram em Chernobyl?',
+    'A natureza se recuperou, com espécies como lobos e ursos retornando. A biodiversidade aumentou, apesar da radiação ainda ser um risco.'
+)
+
+criaCartao(
+    'Chernobyl',
+    'Que medidas de contenção foram tomadas após Chernobyl?',
+    'Foi construído um sarcófago de concreto e, em 2016, uma nova estrutura chamada Novo Sarcófago foi concluída.'
+)
+
+criaCartao(
+    'Chernobyl',
+    'Como a cultura popular retrata Chernobyl?',
+    ' Chernobyl é retratada em filmes e séries, como a minissérie da HBO, focando em impactos humanos e falhas que levaram ao desastre.'
+)
